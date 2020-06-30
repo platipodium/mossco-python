3bf0432188dfde8eba42f90d88aa4ba00ed2b2d2
Carsten Lemmen <carsten.lemmen@hzg.de>
2020-06-30 17:13:02 +0200

    Added language variation (tests fail)

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,15 +5,6 @@ import pytest
 from mossco_python import console
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
-
-@pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
