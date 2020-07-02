4b28320546ceaea757e90dfbe0a966ed5970aafe
Carsten Lemmen <carsten.lemmen@hzg.de>
2020-07-02 15:40:24 +0200

    Added flake8-bugbear and bandit

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,5 @@
+import click.console
 import click.testing
-
-tests / test_console.py
 import pytest
 
 
@@ -10,29 +9,29 @@ def runner():
 
 
 def test_main_succeeds(runner, mock_requests_get):
-    result = runner.invoke(console.main)
+    result = runner.invoke(click.console.main)
     assert result.exit_code == 0
 
 
 def test_main_prints_title(runner, mock_requests_get):
-    result = runner.invoke(console.main)
+    result = runner.invoke(click.console.main)
     assert "Lorem Ipsum" in result.output
 
 
 def test_main_invokes_requests_get(runner, mock_requests_get):
-    runner.invoke(console.main)
+    runner.invoke(click.console.main)
     assert mock_requests_get.called
 
 
 def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
-    runner.invoke(console.main)
+    runner.invoke(click.console.main)
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
 
 
 def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
-    result = runner.invoke(console.main)
+    result = runner.invoke(click.console.main)
     assert result.exit_code == 1
 
 
@@ -49,11 +48,11 @@ def mock_wikipedia_random_page(mocker):
 
 
 def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
-    runner.invoke(console.main, ["--language=pl"])
+    runner.invoke(click.console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
 
 
 @pytest.mark.e2e
 def test_main_succeeds_in_production_env(runner):
-    result = runner.invoke(console.main)
+    result = runner.invoke(click.console.main)
     assert result.exit_code == 0
